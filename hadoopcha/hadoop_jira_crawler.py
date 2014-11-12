58088cd57efbaa1e64060bfa62ff451a614e903b
Long <long@long-ucsd-slave.(none)>
2014-11-11 20:40:29 -0800

    refactoring the crawl code

--- hadoopcha/hadoop_jira_crawler.py
+++ hadoopcha/hadoop_jira_crawler.py
@@ -3,11 +3,17 @@ sys.path.append('..')
 from crawler import Crawler
 
 import urllib2
+import os
+
+from lxml import etree
+from lxml import html
+from bs4 import BeautifulSoup
+
 
 class HadoopJIRACrawler(Crawler):
 
     def crawl(self):
-        for index in range(1, 11294):
+        for index in range(1, 11298):
 	        number = "HADOOP-" + str(index)
 	        url="https://issues.apache.org/jira/browse/" + number
 
@@ -18,4 +24,35 @@ class HadoopJIRACrawler(Crawler):
 	        fwriter.write(data)
 	        fwriter.close()
 
-    def write2log()
+
+    def write2log(self):
+	output = open(self.output_log, 'a')
+	for num in range(1, 11298):
+		case = open(self.output_dir + "/HADOOP-" + str(num), 'r')	
+		html = BeautifulSoup(case)
+			
+		#title part
+		output.write("\n\n-------------------------------------------------------------------------\n")
+		output.write(html.title.string.encode('utf-8'))
+		output.write('\n')
+		
+		#description part
+		descriptions = html.find_all("div", "user-content-block")
+		for description in descriptions:
+			output.write(description.get_text().encode('utf-8').strip())
+		output.write('\n')
+	
+		#comments part
+		comments = html.find_all("div", "action-body flooded")	
+		for comment in comments:
+			output.write(comment.get_text().encode('utf-8'))
+			
+		if num % 10 ==0:
+			print "finish " + str(num)	
+	
+	output.close()		
+
+
+hadoopjiracrawler = HadoopJIRACrawler("/home/long/Research/Conquid/pages", "hadoop.jira.log")
+#hadoopjiracrawler.crawl()
+hadoopjiracrawler.write2log()
