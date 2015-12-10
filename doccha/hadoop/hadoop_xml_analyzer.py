f759a4102e29ef98e8e06f5b8dcd73ef2c86062d
Tianyin Xu <tixu@cs.ucsd.edu>
2015-12-10 15:05:10 -0800

    [EYWK] add the awesome xml finder which can auto dedup and diff

--- doccha/hadoop/hadoop_xml_analyzer.py
+++ doccha/hadoop/hadoop_xml_analyzer.py
@@ -6,22 +6,7 @@ import csv
 import subprocess
 import os
 import fnmatch
-
-def findDefaultXML(repop):
-  """
-  Hadoop's config file is all named XXX-default.xml,
-  and we want to automatically find these config files given a repository
-  """
-  pattern = '*default*.xml'
-  flst = []
-  for dname, sdname, flist in os.walk(repop):
-    for fname in flist:
-      if fnmatch.fnmatch(fname, pattern):
-        if fname.find('test') != -1:
-          print 'Ignore: ', fname
-        else:
-          flst.append(os.path.join(dname, fname))
-  return flst
+import filecmp
 
 def getParamsFromXML(defxml):
   """
@@ -42,15 +27,62 @@ def getParamsFromXML(defxml):
     plst.append(p)
   return plst
 
+def findDefaultXML(repop):
+  """
+  Hadoop's config file is all named XXX-default.xml,
+  and we want to automatically find these config files given a repository
+  """
+  pattern = '*default*.xml'
+  flst = []
+  ignore = []
+  for dname, sdname, flist in os.walk(repop):
+    for fname in flist:
+      if fnmatch.fnmatch(fname, pattern):
+        if fname.find('test') != -1:
+          #print 'Ignore: ', fname
+          ignore.append(fname)
+        elif dname.find('/test') != -1: # it is located in a test dir
+          #print 'Ignore: ', dname, fname
+          pass
+        else:
+          flst.append(os.path.join(dname, fname))
+  return flst, ignore
+
 #TODO: put the parameters of each version into a set,
 #also, we need to handle multiple xmls with the same name
 #also, print out all the xml names so that we can remove some of them
-BASE_REPO_DIR = '/media/tianyin/TOSHIBA EXT/software/hadoop-dist/'
-for repo in os.listdir(BASE_REPO_DIR):
-  repop = os.path.join(BASE_REPO_DIR, repo)
-  if os.path.isdir(repop):
-    print repop, '-----------------------------------------'
-    for f in findDefaultXML(repop):
-        print f
-
-
+def findXMLFiles(repodir):
+  xmlsmap = {}
+  ignored = []
+  for repo in os.listdir(repodir):
+    xmlsmap[repo] = []
+    repop = os.path.join(repodir, repo)
+    if os.path.isdir(repop):
+      print repop, '-----------------------------------------'
+      xml2anyz, ign = findDefaultXML(repop)
+      for ix in ign:
+        if ix not in ignored:
+          ignored.append(ix)
+      #here we want to dedup because sometimes the same files occur multiple times...
+      unqxml2anyz = []
+      for xa in xml2anyz:
+        dup = False
+        for uxa in unqxml2anyz:
+          if os.path.basename(xa) == os.path.basename(uxa):
+            # compare the contents
+            if filecmp.cmp(xa, uxa) == False:
+              print '[ERROR] The two config file has the same name but different contents:'
+              print xa
+              print uxa
+              print '---------------------------------------------------------------------'
+              break
+            else:
+              dup = True
+              break
+        if dup == False:
+          unqxml2anyz.append(xa)
+      print '#dup xmls: ', str(len(xml2anyz) - len(unqxml2anyz)) 
+  for ix in ignored:
+    print 'IGNORED FILE NAMES: ', ix
+            
+findXMLFiles('/media/tianyin/TOSHIBA EXT/software/hadoop-dist/')
