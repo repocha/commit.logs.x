71ae3b66ee0c330795ec470412f5ab47236186fa
Tianyin Xu <tixu@cs.ucsd.edu>
2015-12-11 11:24:43 -0800

    [EYWK] add a jira parser

---
+++ commitcha/jiraparser.py
@@ -0,0 +1,25 @@
+import os
+from lxml import etree
+from lxml.html import fromstring
+from lxml.html import tostring
+import lxml.html as lh
+import csv
+from io import StringIO, BytesIO
+
+
+def parse(jpath):
+  jira = {}
+  f = open(jpath)
+  html = f.read()
+  f.close()
+  doc = fromstring(html)
+  #for title in doc.get_element_by_id('summary-val'):
+  for title in doc.xpath("//h1[@id='summary-val']"):
+    jira['title'] = title.text_content().strip()
+  for summary in doc.find_class('user-content-block'):
+    jira['summary'] = summary.text_content().strip()
+  if 'title' not in jira:
+    print 'ERROR: NO TITLE'
+  return jira
+
+print parse('/media/tianyin/TOSHIBA EXT/tixu_old/longjin/hadoop-jira/HADOOP-/HADOOP-2222')
