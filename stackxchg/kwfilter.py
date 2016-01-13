6db3af264ad00a4c3d438e6caa69c920d5d80e1c
Tianyin Xu <tixu@cs.ucsd.edu>
2016-01-13 14:20:41 -0800

    [PERM] allow the filter to specify multiple keywords

--- stackxchg/kwfilter.py
+++ stackxchg/kwfilter.py
@@ -8,14 +8,20 @@ from lxml.html import fromstring
 import lxml.html as lh
 from StringIO import StringIO
 
-def filterall(dirp, kws):
+def filterall(dirp, kws, known=None):
   for f in os.listdir(dirp):
     ppath = os.path.join(dirp, f)
-    url = filter(ppath, kws)
-    if url != None:
-      print url
+    try:
+      url = filter(ppath, kws)
+      if known != None and url in known:
+        continue
+      if url != None:
+        print url
+    except:
+      print 'FAILURE: ', ppath
+      continue
 
-def filter(pagepath, kws):
+def filter(pagepath, kwss):
   """
   Success: return the url
   Failure: return None
@@ -28,22 +34,48 @@ def filter(pagepath, kws):
   purl = getlink(doc)
   for question in doc.find_class('question'):
     qt = question.text_content().lower()
-    for kw in kws:
-      if kw in qt:
+    for kws in kwss:
+      if containsAll(qt, kws):
         return purl
   for answer in doc.find_class('answer'):
     at = answer.text_content().lower()
-    for kw in kws:
-      if kw in at:
+    for kws in kwss:
+      if containsAll(at, kws):
         return purl
   return None
+
+def containsAll(text, kws):
+  for kw in kws:
+    if kw not in text:
+      return False
+  return True
 
 def getlink(doc):
   for l in doc.iter('link'):
     if l.get('rel') == 'canonical':
       return l.get('href')
 
+permKW = [
+          ['permission', 'deny'],   
+          ['permission', 'denied'], 
+          ['perm', 'deny'],   
+          ['perm', 'denied'], 
+        ]
+
+accessKW = [
+          ['access', 'deny'],
+          ['access', 'denied']
+        ]
+
 #checkAll('/home/xuepeng/everything_about_apache/')
 #checkAll('everything_about_apache_stackoverflow/')
 #print filter('adding-a-reverse-proxy-nginx-or-varnish', ['hello the reverse reverse'])
-filterall('/media/tianyin/TOSHIBA EXT/tixu_old/xuepeng/iconfigure/everything_about_apache', ['permission den'])
+#filterall('/media/tianyin/TOSHIBA EXT/tixu_old/xuepeng/iconfigure/everything_about_apache', accessKW)
+
+#known = []
+#with open('mysql_perm_deny_urls') as f:
+#  for l in f:
+#    known.append(l.strip())
+#print len(known)
+#filterall('/media/tianyin/TOSHIBA EXT/tixu_old/xuepeng/iconfigure/everything_about_mysql', accessKW)
+filterall('/media/tianyin/TOSHIBA EXT/tixu_old/xuepeng/iconfigure/everything_about_mysql', permKW)
