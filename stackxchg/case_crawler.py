e067710ad9e0c67996bd86bb5bcc9228fba26417
Tianyin Xu <tixu@cs.ucsd.edu>
2016-01-28 18:00:46 -0800

    [STUD] support wildcards in crawler

--- stackxchg/case_crawler.py
+++ stackxchg/case_crawler.py
@@ -9,25 +9,35 @@ SECTION = 'tagged_url_crawler'
 conf = config.getconfig()
 
 url_list = conf.get(SECTION, 'post_list')
-case_dir = conf.get(SECTION, 'case_dir_template')
+case_dir_template = conf.get(SECTION, 'case_dir_template')
 
 #URL_LIST = 'serverfault_tags_postgresql_questions.lst'
 #CASE_DIR = '/media/tianyin/TOSHIBA EXT/tixu_old/xuepeng/iconfigure/everything_about_postgresql'
 
-tag = raw_input('Enter the tag name: ')
+rawtag = raw_input('Enter the tag name: ')
 
-case_dir = case_dir.replace('TAG', tag) 
-if os.path.exists(case_dir) == False:
-  os.makedirs(case_dir)
-print 'Download to', case_dir
+tags = []
+if '*' in rawtag:
+  key = rawtag.replace('*', '')
+  for f in os.listdir('.'):
+    if 'serverfault_tags_' in f and '_questions.lst' in f and key in f:
+      tags.append(f.replace('serverfault_tags_', '').replace('_questions.lst', ''))
+  print tags
+else:
+  tags.append(rawtag)
 
-urls = []
-with open(url_list.replace('TAG', tag)) as f:
-  for l in f:
-    urls.append(l.strip())
+for tag in tags:
+  case_dir = case_dir_template.replace('TAG', tag) 
+  if os.path.exists(case_dir) == False:
+    os.makedirs(case_dir)
+  print 'Download to', case_dir
 
-crawler = URLCrawler(urls, case_dir, None)
-crawler.calibrate()
-crawler.crawl()
+  urls = []
+  with open(url_list.replace('TAG', tag)) as f:
+    for l in f:
+      urls.append(l.strip())
 
-print 'FINISH CRAWLING TAG \'' + tag + '\''
+  crawler = URLCrawler(urls, case_dir, None)
+  crawler.calibrate()
+  crawler.crawl()
+  print 'FINISH CRAWLING TAG \'' + tag + '\''
