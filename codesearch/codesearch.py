455caefc9992e227849a67e218ef99bceef582d0
Tianyin Xu <tixu@cs.ucsd.edu>
2016-05-18 22:41:18 -0700

    add the summary output

--- codesearch/codesearch.py
+++ codesearch/codesearch.py
@@ -4,6 +4,7 @@ from cha import GitCha
 import subprocess
 import os
 import re
+import csv
 
 #REPO = '/home/tianyin/hadoop-trunk/hadoop'
 REPO = '/home/tianyin/httpd-trunk/httpd'
@@ -27,19 +28,23 @@ gitcha.parse(CLOG)
 print len(gitcha.charepo)
 
 i = 0
+csvf = open('summary.csv', 'wb')
+writer = csv.writer(csvf)
 for c in gitcha.charepo:
   commit = c['version']
   out, err = show_commit_code(commit)
   for logstmt in logstmts:
     if logstmt in out:
-      #print c['changes']
       i += 1
+      writer.writerow([str(i),c['changes']])
+      #print c['changes']
       op = os.path.join('httpd_commits', str(i))
       with open(op, 'w') as f:
         f.write(out)
       #print commit
       break
 print '# selected commits:', i
+
   #if 'throw new AccessControlException' in out:
   #  print c['changes']
   
