534ed14a3bbe00df711c3a9ed229422deef56094
Tianyin Xu <tixu@cs.ucsd.edu>
2016-05-19 01:27:30 -0700

    split the logics

--- codesearch/codesearch.py
+++ codesearch/codesearch.py
@@ -6,44 +6,33 @@ import os
 import re
 import csv
 
-#REPO = '/home/tianyin/hadoop-trunk/hadoop'
-REPO = '/home/tianyin/httpd-trunk/httpd'
-CLOG = 'aaa.commit.log'
-
-logstmts = ['ap_log_perror',
-            'ap_log_error',
-            'ap_log_rerror',
-            'ap_log_cerror',
-            'ap_log_cserror'
-           ]
-
 def show_commit_code(commit):
   cmd = ['git', '-C', REPO, 'show', commit]
   process = subprocess.Popen(cmd, stdout = subprocess.PIPE, stderr = subprocess.PIPE) 
   out, err = process.communicate()
   return out, err
 
-gitcha = GitCha()
-gitcha.parse(CLOG)
-print len(gitcha.charepo)
-
-i = 0
-csvf = open('summary.csv', 'wb')
-writer = csv.writer(csvf)
-for c in gitcha.charepo:
-  commit = c['version']
-  out, err = show_commit_code(commit)
-  for logstmt in logstmts:
-    if logstmt in out:
-      i += 1
-      writer.writerow([str(i),c['changes'],c['version']])
-      #print c['changes']
-      op = os.path.join('httpd_commits', str(i))
-      with open(op, 'w') as f:
-        f.write(out)
-      #print commit
-      break
-print '# selected commits:', i
+def select_commits(sw, commit_log, repo, signatures, outdir):
+  gitcha = GitCha()
+  gitcha.parse(CLOG)
+  print '#commits in logs:', len(gitcha.charepo)
+  i = 0
+  csvf = open(sw + '_summary.csv', 'wb')
+  writer = csv.writer(csvf)
+  for c in gitcha.charepo:
+    commit = c['version']
+    out, err = show_commit_code(commit)
+    for s in signatures:
+      if s in out:
+        i += 1
+        writer.writerow([str(i),c['changes'],c['version']])
+        #print c['changes']
+        op = os.path.join(outdir, str(i))
+        with open(op, 'w') as f:
+          f.write(out)
+        #print commit
+        break
+  print '# selected commits:', i
 
   #if 'throw new AccessControlException' in out:
   #  print c['changes']
