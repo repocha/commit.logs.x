5bc2509d406c89a42d756e803237ac56f9100e9c
Tianyin Xu <tixu@cs.ucsd.edu>
2016-05-18 21:59:56 -0700

    remove the url crawler stuff

--- stackxchg/perm_tags.py
+++
@@ -1,9 +0,0 @@
-import tag_crawler
-
-#keywords = ['permission', 'access', 'privilege']
-keywords = ['permission']
-tstats = tag_crawler.aggrTagStats()
-for t in tstats:
-  for kw in keywords:
-    if kw in t:
-      print t, tstats[t]
