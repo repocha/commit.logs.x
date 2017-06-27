cee33987933c05359c622e2ecfcdf8d1a1bc7a42
Tianyin Xu <xuty1985@gmail.com>
2017-06-27 12:43:35 -0700

    rm all the BS

--- commitcha/mysqlcha/mysqlcha.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append('..')
-from cha import GitCha
-
-mysqlcha = GitCha()
-mysqlcha.parse('mysql.git.log')
-mysqlcha.getplist('mysql.p.all.2')
-res = mysqlcha.select(mysqlcha.charepo, ['config'] + mysqlcha.plist)
-mysqlcha.print2csv(res, 'mysql.git.cha.csv.2')
